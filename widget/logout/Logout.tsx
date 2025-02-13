010097d39878cb229d83222467a6e98f8e0be754
arkaitzsilva <arkaitz.develop@gmail.com>
2025-02-13 16:18:17 +0100

    logout changes + flake update

--- widget/logout/Logout.tsx
+++ widget/logout/Logout.tsx
@@ -6,38 +6,47 @@ function hide() {
 }
 
 export default function Logout() {
+    const { TOP, BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor
     const { CENTER } = Gtk.Align
-    const width = Variable(1000)
 
     return <window
         name="logout"
-        anchor={Astal.WindowAnchor.TOP | Astal.WindowAnchor.BOTTOM}
-        exclusivity={Astal.Exclusivity.IGNORE}
-        keymode={Astal.Keymode.ON_DEMAND}
+        anchor={TOP | BOTTOM | LEFT | RIGHT}
+        exclusivity={Astal.Exclusivity.EXCLUSIVE}
+        keymode={Astal.Keymode.EXCLUSIVE}
         application={App}
         visible={false}
         onKeyPressEvent={function (self, event: Gdk.Event) {
             if (event.get_keyval()[1] === Gdk.KEY_Escape)
                 self.hide()
         }}>
-        <box>
-            <eventbox widthRequest={width(w => w / 2)} expand onClick={hide} />
-            <box hexpand={false} vertical>
-                <eventbox heightRequest={100} onClick={hide} />
-                <box widthRequest={100} className="Logout" vertical halign={CENTER} valign={CENTER}>
-                    <button>
-                        <box
-                            halign={CENTER}
-                            className="action"
-                            vertical>
+        <box hexpand={false} vertical>
+            <eventbox expand onClick={hide} />
+            <box 
+                widthRequest={100}
+                className="Logout"
+                horizontal
+                halign={CENTER} >
+                <button>
+                    <box
+                        halign={CENTER}
+                        className="action"
+                        vertical>
                             <icon icon="system-shutdown" />
                             <label label="Apagar" />
-                        </box>
-                    </button>
-                </box>
-                <eventbox expand onClick={hide} />
+                    </box>
+                </button>
+                <button>
+                    <box
+                        halign={CENTER}
+                        className="action"
+                        vertical>
+                            <icon icon="system-reboot" />
+                            <label label="Reiniciar" />
+                    </box>
+                </button>
             </box>
-            <eventbox widthRequest={width(w => w / 2)} expand onClick={hide} />
+            <eventbox expand onClick={hide} />
         </box>
     </window>
 }
